4a06aa96f04b9cf938df6d037481cf7e3e586f27
Carson-Tunnell <carsontunnell@hotmail.com>
2017-04-24 12:18:13 -0600

    Update Driver.cc

--- Driver.cc
+++ Driver.cc
@@ -1,7 +1,8 @@
 #include <string>
 #include <iostream>
 #include <map>
-
+//Must Use Wiring pi to initialize system and read data from pins
+//http://wiringpi.com/reference/setup/
 using namespace std;
 
 

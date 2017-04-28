eb6a82ba3a7d950adafccdbcca2a5ce3a4629821
Carson-Tunnell <carsontunnell@hotmail.com>
2017-04-28 14:22:51 -0600

    Update temp_sensor.h
    updated code to have return values and comments to explain methods

--- temp_sensor.h
+++ temp_sensor.h
@@ -4,9 +4,9 @@
 class temp_sensor
 {
     public:
-        temp_sensor(int);    
-        get_temp();
-        get_humid();
+       double temp_sensor(int);    // call once to set the pin for the temp sensor
+       double get_temp();          //returns a double with the current temp
+       double get_humid();         //returns a double with the current humidity
     private:
         int pin;
 }
